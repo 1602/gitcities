ab38cfb9c92b66aa9302427d8f72a741675f17ce
Anatoliy Chakkaev <mail@anatoliy.in>
2013-11-09 14:38:10 +0000

    Fix style, global vars

--- app/controllers/repo.js
+++ app/controllers/repo.js
@@ -1,22 +1,20 @@
-module.exports = RepoController
-function RepoController(){}
-RepoController.prototype.show = function( c ) {
-  name = c.params.username + '/' + c.params.reponame
-  this.title = name;
-  c.Repository.findOne(
-    {where: {name: name}},
-    function(force, data) {
-      if(data == null) {
-        c.Repository.create(
-          {name: name},
-          function(force, data) {
-            c.render('show', {repo: data});
-          }
-        );
-      } else {
-        c.render('show', {repo: data});
-      }
-    }
-  );
-
+module.exports = RepoController;
+function RepoController(){
 }
+
+RepoController.prototype.show = function( c ) {
+    var name = c.params.username + '/' + c.params.reponame;
+    this.title = name;
+    c.Repository.findOne({where: {name: name}}, function(err, data) {
+        if (err) {
+            c.next(err);
+        }
+        if (!data) {
+            c.Repository.create({name: name}, function(force, data) {
+                c.render('show', {repo: data});
+            });
+        } else {
+            c.render('show', {repo: data});
+        }
+    });
+};
