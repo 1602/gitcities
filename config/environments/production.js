5785e0e59505a03d829e6389899c0344efeb9d3a
Anatoliy Chakkaev <mail@anatoliy.in>
2013-11-10 22:33:53 +0000

    Rotation

--- config/environments/production.js
+++ config/environments/production.js
@@ -4,6 +4,19 @@ module.exports = function (compound) {
     var app = compound.app;
 
     app.configure('production', function () {
+
+        Repository.processNext(function again() {
+            setTimeout(function() {
+                Repository.processNext(again);
+            }, 200);
+        });
+
+        User.processNext(function anotherUser() {
+            setTimeout(function() {
+                User.processNext(anotherUser);
+            }, 200);
+        });
+
         app.enable('quiet');
         app.enable('merge javascripts');
         app.enable('merge stylesheets');
