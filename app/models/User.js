8492451e023a30f5b7dfa52e4ef858724cf1968f
Anatoliy Chakkaev <mail@anatoliy.in>
2013-11-10 14:08:18 +0000

    Detailed karma

--- app/models/User.js
+++ app/models/User.js
@@ -23,6 +23,57 @@ module.exports = function(compound, User) {
         });
     };
 
+    User.prototype.explainKarma = function(cb) {
+        var now = Date.now();
+        var karma = {
+            week: {a: 0, c: 0, p: {}},
+            month: {a: 0, c: 0, p: {}},
+            later: {a: 0, c: 0, p: {}}
+        };
+        var projectIds = {};
+        var ownRepos = this.authorOf.map(function(x) {
+            return x.id;
+        });
+        Commit.all({min: Date.now() - 86400000 * 108, max: Date.now(), where: {userId: this.id}}, function(err, cs) {
+
+            cs.forEach(function(c) {
+                var daysAgo = Math.ceil((now - c.date.getTime()) / 86400000);
+                if (daysAgo <= 1) daysAgo = 2;
+                var ca, cc, type;
+                if (ownRepos.indexOf(c.repoId) === -1) {
+                    type = 'contributor';
+                    ca = 10;
+                    cc = 50;
+                } else {
+                    type = 'author';
+                    cc = 10;
+                    ca = 50;
+                }
+                var gravity = Math.log(daysAgo);
+                var period = 'later';
+                if (daysAgo <= 7) {
+                    period = 'week';
+                } else if (daysAgo <= 31) {
+                    period = 'month';
+                }
+                karma[period].a += c.gainA = Math.round(ca / gravity);
+                karma[period].c += c.gainC = Math.round(cc / gravity);
+                c.gain = c.gainC + c.gainA;
+                karma[period].p[c.repoId] = karma[period].p[c.repoId] || [];
+                karma[period].p[c.repoId].push(c);
+                projectIds[c.repoId] = null;
+            });
+            Repository.all({where: {id: {inq: Object.keys(projectIds)}}}, function(err, projects) {
+                projects.forEach(function(x) {
+                    projectIds[x.id] = x;
+                });
+                karma.projects = projectIds;
+                console.log(karma.projects);
+                cb(karma);
+            });
+        });
+    };
+
     User.prototype.calcKarma = function(cb) {
         var ka = 0, kc = 0, now = Date.now(), user = this;
         var ownRepos = this.authorOf.map(function(x) {
