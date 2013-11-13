7100720a6ccf667455cea9907f2fc1ac1021d26e
Anatoliy Chakkaev <mail@anatoliy.in>
2013-11-13 22:33:32 +0000

    Use login in gh name as well

--- app/helpers/application_helper.js
+++ app/helpers/application_helper.js
@@ -1,7 +1,8 @@
 exports.makeGutHubLink = function(s) {
+    if ('undefined' === typeof s) return '';
     var link = s;
     if (typeof s !== 'string') {
-        link = s.username || s.name;
+        link = s.username || s.login || s.name;
         s = s.name;
     }
     return '<a href="https://github.com/' + link + '">' + s + '</a>';
