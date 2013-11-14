9bb27fedd2378fb5ab55785d80991dbfdc27a296
Anatoliy Chakkaev <mail@anatoliy.in>
2013-11-14 19:47:08 +0000

    Repo show page

--- app/models/Contribution.js
+++ app/models/Contribution.js
@@ -1,4 +1,8 @@
 module.exports = function(compound, Contribution) {
     Contribution.belongsTo('user');
     Contribution.belongsTo('repository');
+
+    Contribution.prototype.formatWeight = function() {
+        return this._weight ? Math.round(this.weight * 100) / 100 : 0;
+    };
 };
